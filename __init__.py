e66c6ffb6fefda587bcb6eda4529369f115dd693
Pancakem <pancakesdeath@protonmail.com>
2019-06-19 09:52:05 +0300

    change boilerplate

--- __init__.py
+++ __init__.py
@@ -6,18 +6,14 @@ from flask import Blueprint
 
 from main.controller.user_controller import api as user_ns
 from main.controller.auth_controller import api as auth_ns
-from main.controller.product_controller import api as product_ns
-from main.controller.productsale_controller import api as productsale_ns
 
 blueprint = Blueprint('api', __name__)
 
 api = Api(blueprint,
-	title='Fiucha inventory management testing',
+	title='SoftSearch interview',
 	version='1.0',
-	description='a boilerplate for fiucha web service'
+	description='a boilerplate for an example web service'
 	)
 
 api.add_namespace(user_ns, path='/user')
 api.add_namespace(auth_ns)
-api.add_namespace(product_ns)
-api.add_namespace(productsale_ns)
