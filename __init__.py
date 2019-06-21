1d5598a20e18bb53eb735a8408267be9709fa412
Pancakem <pancakesdeath@protonmail.com>
2019-06-21 16:11:48 +0300

    add record namespace to swagger

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ from flask import Blueprint
 
 from main.controller.user_controller import api as user_ns
 from main.controller.auth_controller import api as auth_ns
+from main.controller.record_controller import api as record_ns
 
 blueprint = Blueprint('api', __name__)
 
@@ -17,3 +18,4 @@ api = Api(blueprint,
 
 api.add_namespace(user_ns, path='/user')
 api.add_namespace(auth_ns)
+api.add_namespace(record_ns)
