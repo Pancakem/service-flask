2ce5e98288785f78112851a3d2d3962b1b96d094
Pancakem <pancakesdeath@protonmail.com>
2019-06-21 15:17:42 +0300

    add record dto

--- main/util/dto.py
+++ main/util/dto.py
@@ -13,9 +13,19 @@ class UserDto:
 
 
 class AuthDto:
-	api = Namespace("auth", description="authentication related operations")
-	user_auth = api.model("auth_details",{
-		"email": fields.String(required=True, description="The email address"),
-		"password": fields.String(required=True, description="The user password"),
+	api = Namespace('auth', description='authentication related operations')
+	user_auth = api.model('auth_details',{
+		'email': fields.String(required=True, description='The email address'),
+		'password': fields.String(required=True, description='The user password'),
 	}
+	)
+
+class RecordDto:
+	api = Namespace('record', description='record related operations')
+	record = api.model(
+		'record', {
+			'name': fields.String(required=True, description='The record name'),
+			'first_part': fields.String(required=True, description='Record body')
+			'second_part': fields.String(description='Optional record info')
+		}
 	)
