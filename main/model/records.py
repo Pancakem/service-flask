c04bcda1e753b5062b5d8d96d653ad2107fc5355
Pancakem <pancakesdeath@protonmail.com>
2019-06-21 10:17:54 +0300

    add foreign key to user

--- main/model/records.py
+++ main/model/records.py
@@ -4,6 +4,12 @@ from .. import db
 class Record(db.Model):
     __tablename__ = "records"
 
-    id = db.Column(db.Integer)
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, unique=True)
     first_part = db.Column(db.String(100))
     second_part = db.Column(db.String(100))
+    date_created = db.Column(db.DateTime)
+    user_id = db.Column(db.String(100), db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<record : {self.name}>'
