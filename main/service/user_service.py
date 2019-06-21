78d1c4eed3d0733c18b5cbb6faea5163824da40f
Pancakem <pancakesdeath@protonmail.com>
2019-06-21 13:46:40 +0300

    name CRUD functions appropriately

--- main/service/user_service.py
+++ main/service/user_service.py
@@ -12,7 +12,7 @@ def generate_token(user):
 		response_object = {
 			"status": "succes",
 			"message": "Successfully registered.",
-			"Authorization": user.decode_auth_token(auth_token)
+			"Authorization": auth_token
 		}
 
 		return response_object, 201
@@ -26,7 +26,7 @@ def generate_token(user):
 		return response_object, 401
 
 # this function should only be accessed by a logged in user
-def save_new_user(data):
+def create_user(data):
 	user = User.query.filter_by(email=data['email']).first()
 
 	if not user:
@@ -36,8 +36,7 @@ def save_new_user(data):
 			first_name=data['first_name'],
 			last_name=data['last_name'],
 			password=data['password'],
-			date_registered=datetime.datetime.utcnow(),
-			is_admin = data['is_admin']
+			date_registered=datetime.datetime.utcnow()
 			)
 
 		save_changes(new_user)
@@ -55,9 +54,48 @@ def get_a_user(public_id):
 	return User.query.filter_by(public_id=public_id).first()
 
 # only an admin or super admin can view all users
-def get_all_users(admin_token):
+def get_all_users():
 	return User.query.all()
 
+def create_admin(data):
+	admin = User.query.filter_by(email=data['email']).first()
+
+	if not admin:
+		new_admin = User(
+			public_id=str(uuid.uuid4()),
+			email=data['email'],
+			first_name=data['first_name'],
+			last_name=data['last_name'],
+			password=data['password'],
+			date_registered=datetime.datetime.utcnow(),
+			is_admin = 1
+		)
+		save_changes(new_admin)
+		return generate_token(new_admin)
+	else:
+
+		response_object = {
+			'status': 'fail',
+			'message': 'User already exists. Please Log in.'
+		}
+
+		return response_object, 409
+
+def delete_user(data):
+	user = User.query.filter_by(email=data['email']).first_or_404()
+	
+def delete_user_by_id(id):
+	user = User.query.filter_by(public_id=data['id']).first_or_404()
+
+def update_user(data):
+	user = User.query.filter_by(public_id=data['id']).first_or_404()
+	user.email=data['email']
+	user.first_name = data['first_name']
+	user.last_name = data['last_name']
+	
+	db.session.update(user)
+	db.session.commit()
+	
 
 def save_changes(data):
 	db.session.add(data)
