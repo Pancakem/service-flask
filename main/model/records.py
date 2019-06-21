e698f0655e53710d551916e65eb7e3fcf83fe79c
Pancakem <pancakesdeath@protonmail.com>
2019-06-21 21:43:43 +0300

    rename record fields

--- main/model/records.py
+++ main/model/records.py
@@ -5,11 +5,10 @@ class Record(db.Model):
     __tablename__ = "records"
 
     id = db.Column(db.String, primary_key=True)
-    name = db.Column(db.String, unique=True)
-    first_part = db.Column(db.String(100))
-    second_part = db.Column(db.String(100))
+    title = db.Column(db.String, unique=True)
+    body = db.Column(db.String(1000))
     date_created = db.Column(db.DateTime)
     user_id = db.Column(db.String(100), db.ForeignKey('user.public_id'), nullable=False)
 
     def __repr__(self):
-        return f'<record : {self.name}>'
+        return f'<record : {self.title}>'
