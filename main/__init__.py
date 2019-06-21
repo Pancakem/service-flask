56c509ee44ced6e6f456dcd38a44baa15202409e
Pancakem <pancakesdeath@protonmail.com>
2019-06-21 20:38:43 +0300

    enable CORS

--- main/__init__.py
+++ main/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS, cross_origin
 
 from .config import config_by_name
 
@@ -12,6 +13,7 @@ def create_app(config_name):
 	app.config.from_object(config_by_name[config_name])
 	db.init_app(app)
 	bcrypt.init_app(app)
-
+	cors = CORS(app)
+	app.config['CORS_HEADERS'] = 'Content-Type'
 
 	return app
